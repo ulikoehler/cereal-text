95b37d7ddba6b3734472093630a5e93d673e3035
Uli Köhler <ulikoehler@online.de>
2014-04-26 02:04:35 +0200

    Initial version with instances for Text and lazy Text

---
+++ Data/Serialize/Text.hs
@@ -0,0 +1,21 @@
+{-|
+    Data.Text instances for cereal.
+
+    Provides instances for Text and lazy Text
+-}
+module Data.Serialize.Text () where
+
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Serialize
+import Control.Applicative ((<$>))
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.Lazy.Encoding as TLE
+
+instance Serialize T.Text where
+    put = put . TE.encodeUtf8
+    get = TE.decodeUtf8 <$> get 
+
+instance Serialize LT.Text where
+    put = put . TLE.encodeUtf8
+    get = TLE.decodeUtf8 <$> get 
